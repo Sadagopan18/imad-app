864c831f6b3edbd54ddc198a7f913e09621b7491
Sadagopan18 <30694139+Sadagopan18@users.noreply.github.com>
2017-08-05 15:17:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get("/article-one",function(req,res){
-   res.sendFile("Article one will be server here"); 
+   res.send("Article one will be server here"); 
 });
     
 app.get("/article-two",function(req,res){
-    res.sendFile("Article two will be server here");
+    res.send("Article two will be server here");
     
 });
 app.get("/article-three",function(req,res){
-   res.sendFile("Article-three will be server here"); 
+   res.send("Article-three will be server here"); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
